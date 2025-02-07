22e86f13a132a435955a5c5683f062ee88af5e8a
ErikBetzholtz <erik.betzholtz@gmail.com>
2025-02-07 18:13:00 +0100

    Squashed commit of the following:
    commit e5c316ec5b9790b8cdf63cf058d0340eb0cfef94
    Author: RasmussBrodin <rasmuss.brodin@gmail.com>
    Date:   Fri Feb 7 14:37:10 2025 +0100
    
        feat: complete #10
    
        Changed to use maven, will be easier for testing i think.
    
    commit 5534a2be1b9d36d38b6f7368b3b07a4e9ff9096e
    Author: Julia Potrus <juliapo@kth.se>
    Date:   Fri Feb 7 12:12:01 2025 +0100
    
        feat: complete issue #1

--- src/main/java/se/kth/ContinuousIntegrationServer.java
+++ src/main/java/se/kth/ContinuousIntegrationServer.java
@@ -24,30 +24,34 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     static String buildDirectory = ".serverbuild";
 
     public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
+            Request baseRequest,
+            HttpServletRequest request,
+            HttpServletResponse response)
             throws IOException {
         response.setContentType("text/html;charset=utf-8");
         baseRequest.setHandled(true);
-        // For full documentation what GitHub sends, read https://docs.github.com/en/webhooks/webhook-events-and-payloads
-        // We are most interested in https://docs.github.com/en/webhooks/webhook-events-and-payloads#push
+        // For full documentation what GitHub sends, read
+        // https://docs.github.com/en/webhooks/webhook-events-and-payloads
+        // We are most interested in
+        // https://docs.github.com/en/webhooks/webhook-events-and-payloads#push
 
-        // What we can also do it use target for the webserver to host content. Eg target == index.html responds with html
+        // What we can also do it use target for the webserver to host content. Eg
+        // target == index.html responds with html
         System.out.println(target);
-
 
         try {
             // 1st clone your repository
 
             // TODO not hardcode by for example using .repository in webhook push json
-            String repository = "https://github.com/VincentLagerros/DD2480-Group-5-Assignment-2/";
-            String branch = "testing";
+            String repository = "git@github.com:Juliapp123/test.git";
+            String branch = "main";
+            //String file = ".serverbuild/Main.java";
 
             cloneRepository(repository, branch, buildDirectory);
-
+            printRepo(buildDirectory);
+           
             // 2nd compile the code
-            // TODO compile
+            compileRepository();
 
             response.getWriter().println("CI job done");
             response.setStatus(HttpServletResponse.SC_OK);
@@ -70,16 +74,19 @@ public class ContinuousIntegrationServer extends AbstractHandler {
      */
     static void cloneRepository(String url, String branch, String directory) throws InterruptedException, IOException {
         try {
-            // just delete the file directory in case it exists for a clean git clone, this is easier than git pull
+            // just delete the file directory in case it exists for a clean git clone, this
+            // is easier than git pull
             FileUtils.deleteDirectory(new File(directory));
         } catch (Throwable _) {
             // ignore if we can even delete it or not
         }
 
-        // spawn the process for git cloning and wait, this is easier than importing a lib
+        // spawn the process for git cloning and wait, this is easier than importing a
+        // lib
         Process process = new ProcessBuilder("git", "clone", url, "-b", branch, directory).start();
         if (process.waitFor() != 0) {
-            throw new IOException("Bad exitcode (" + process.exitValue() + ") in git clone due to: " + new String(process.getErrorStream().readAllBytes()));
+            throw new IOException("Bad exitcode (" + process.exitValue() + ") in git clone due to: "
+                    + new String(process.getErrorStream().readAllBytes()));
         }
     }
 
@@ -92,4 +99,37 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     static Result runTests(Class<?> c){  
        return new JUnitCore().run(c);
     }
+    
+    /**
+     * Compiles the cloned repository
+     *
+     * @param file      the cloned file to compile
+     */
+    static void compileRepository() throws InterruptedException, IOException {
+        // spawn the process for compiling (and running?) and wait
+        Process process = new ProcessBuilder("mvn", "compile")
+            .directory(new File(buildDirectory)) // same as  "cd .serverbuild mvn compile" have to go to the correct dir
+            .start();
+        if (process.waitFor() != 0) {
+            throw new IOException("Error: (" + process.exitValue() + ") in complilation"
+                    + new String(process.getErrorStream().readAllBytes()));
+        }
+    }
+
+    static void printRepo(String buildDirectory){
+        File folder = new File(buildDirectory);
+        File[] listOfFiles = folder.listFiles();
+        if (listOfFiles != null) {
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    System.out.println("File " + listOfFiles[i].getName());
+                } else if (listOfFiles[i].isDirectory()) {
+                    System.out.println("Directory " + listOfFiles[i].getName());
+                }
+            }
+        }
+
+    }
+    
+
 }
